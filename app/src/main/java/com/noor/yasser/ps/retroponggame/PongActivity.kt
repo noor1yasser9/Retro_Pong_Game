73e24214ae28eeda88d84373b56a2ec2c7b28771
Noor  Yasser <mrh61037@gmail.com>
2021-08-30 11:11:02 +0300

    add Anthore Activity

--- app/src/main/java/com/noor/yasser/ps/retroponggame/PongActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/retroponggame/PongActivity.kt
@@ -1,11 +1,12 @@
 package com.noor.yasser.ps.retroponggame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class PongActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
+
 }
