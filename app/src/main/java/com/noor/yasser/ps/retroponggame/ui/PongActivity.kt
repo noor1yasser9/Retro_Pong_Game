1520904ba8f09bfb4d0f0c96ae4433d8cf885cab
Noor  Yasser <mrh61037@gmail.com>
2021-09-03 10:27:09 +0300

    Update Pong Activity

--- app/src/main/java/com/noor/yasser/ps/retroponggame/ui/PongActivity.kt
+++ app/src/main/java/com/noor/yasser/ps/retroponggame/ui/PongActivity.kt
@@ -1,14 +1,27 @@
 package com.noor.yasser.ps.retroponggame.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.noor.yasser.ps.retroponggame.R
+import com.noor.yasser.ps.retroponggame.model.PongTable
+import com.noor.yasser.ps.retroponggame.utils.GameThread
 
 class PongActivity : AppCompatActivity() {
-
+    private var mGameThread: GameThread? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pong)
+
+
+        val table: PongTable = findViewById<View>(R.id.pongTable) as PongTable
+        table.setScoreOpponent(findViewById<View>(R.id.tvScoreOpponent) as TextView)
+        table.setScorePlayer(findViewById<View>(R.id.tvScorePlayer) as TextView)
+        table.setStatus(findViewById<View>(R.id.tvStatus) as TextView)
+
+
+        mGameThread = table.game
     }
 
 }
