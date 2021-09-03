8e24ec4759430a7f4c3f1dcd2d692e5eeaffb0fe
Noor  Yasser <mrh61037@gmail.com>
2021-09-03 10:14:17 +0300

    Update Ball function radius ball update Player function player

--- app/src/main/java/com/noor/yasser/ps/retroponggame/model/Ball.kt
+++ app/src/main/java/com/noor/yasser/ps/retroponggame/model/Ball.kt
@@ -2,17 +2,14 @@ package com.noor.yasser.ps.retroponggame.model
 
 import android.graphics.Canvas
 import android.graphics.Paint
+import com.noor.yasser.ps.retroponggame.utils.PHY_BALL_SPEED
 
-data class Ball(
-    var cx: Float = 0f,
-    var cy: Float = 0f,
-    var velocity_x: Float = 0f,
-    var velocity_y: Float = 0f,
-    var radius: Int = 0,
-    val paint: Paint
-) {
-
-    public fun draw(canvas: Canvas) {
+class Ball(val radius: Float, private val paint: Paint) {
+    var cx = 0f
+    var cy = 0f
+    var velocity_x: Float = PHY_BALL_SPEED
+    var velocity_y: Float = PHY_BALL_SPEED
+    fun draw(canvas: Canvas) {
         canvas.drawCircle(cx, cy, radius.toFloat(), paint)
     }
 
@@ -21,8 +18,13 @@ data class Ball(
         cy += velocity_y
         if (cy < radius) {
             cy = radius.toFloat()
-        } else if (cy + radius > canvas.height) {
+        } else if (cy + radius >= canvas.height) {
             cy = (canvas.height - radius - 1).toFloat()
         }
     }
+
+    override fun toString(): String {
+        return "Cx = " + cx + "Cy" + cy + "velX= " + velocity_x + " velY= " + velocity_y
+    }
+
 }
--- app/src/main/java/com/noor/yasser/ps/retroponggame/model/Player.kt
+++ app/src/main/java/com/noor/yasser/ps/retroponggame/model/Player.kt
@@ -7,12 +7,12 @@ import android.graphics.RectF
 data class Player(
     val requestWidth: Int,
     val requestHeight: Int,
-    val score: Int = 0,
+    var score: Int = 0,
     val paint: Paint,
     val bounds: RectF = RectF(0f, 0f, requestWidth.toFloat(), requestHeight.toFloat())
 ) {
 
-    public fun draw(canvas: Canvas) {
+     fun draw(canvas: Canvas) {
         canvas.drawRoundRect(bounds, 5f, 5f, paint)
     }
 
